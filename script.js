67eb051aadc654650d5b2ae7f44103e088ecdf8c
Aryanmaha <dmonryner@gmail.com>
2023-11-29 23:05:39 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const wrapper = document.querySelector(".main");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".cute-gif");
+const yesBtn = document.querySelector(".yes-button");
+const noBtn = document.querySelector(".no-button");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Yay, see you tommorow!!";
+  gif.src =
+    "https://media.tenor.com/LxggFGxwOjIAAAAC/shy-cat.gif";
+
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
